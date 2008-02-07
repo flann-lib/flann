d675d0106f64e42ac2a220a5062a03181b947b67
Marius Muja <mariusm@chifeng.cs.ubc.ca>
2008-02-06 16:37:30 -0800

    Worked on the matlab bindings.

---
+++ src/bindings/matlab/nn_free_index.m
@@ -0,0 +1,8 @@
+function nn_free_index(index_id)
+%NN_FREE_INDEX  Deletes the nearest-neighbors index
+%
+% nn_free_index(index_id) deletes the index referenced by the 'index_id'
+% parameter. This index has to have been constructed by the nn_build_index
+% function.
+
+nearest_neighbors('free_index',index_id);
