flann-lib/flann

commit 739916a7717b2a8ea965da654ab1fdfddb596437

Bug fix. (thanks Frédéric Devernay)

src/cpp/flann/flann.hpp
@@ -216,7 +216,7 @@ int Index<Distance>::radiusSearch(const Matrix<ElementType>& query, Matrix<int>&
     }
     RadiusResultSet<DistanceType> result_set(radius, indices_ptr, dists_ptr, n);
     nnIndex->findNeighbors(result_set, query[0], searchParams);
-    size_t cnt = result_set.size();
+    size_t cnt = std::min(result_set.size(), (size_t)n);
     if (n > 0 && searchParams.sorted) {
         std::sort(make_pair_iterator(dists_ptr, indices_ptr),
                   make_pair_iterator(dists_ptr + cnt, indices_ptr + cnt),

commit 18de02993a8bfd9188eaed6da43e40c62f335794

Fix bug introduced in 739916a

src/cpp/flann/flann.hpp
@@ -223,7 +223,7 @@ int Index<Distance>::radiusSearch(const Matrix<ElementType>& query, Matrix<int>&
                   pair_iterator_compare<DistanceType*, int*>());
     }
 
-    return cnt;
+    return result_set.size();
 }
 
 