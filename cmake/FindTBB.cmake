f56ccf6da4cb587c639eb61c44a9658126c0d6a0
Marius Muja <mariusm@cs.ubc.ca>
2011-12-18 07:49:45 +0200

    CMakeFiles TBB fixes

--- cmake/FindTBB.cmake
+++ cmake/FindTBB.cmake
@@ -18,7 +18,7 @@ find_package(PkgConfig)
 pkg_check_modules(PC_TBB tbb)
 
 # Find include directory
-find_path(TBB_INCLUDE_DIR tbb/task_scheduler_init.h
+find_path(TBB_INCLUDE_DIR tbb/task_scheduler_init.h 
     HINTS ${PC_TBB_INCLUDEDIR} ${PC_TBB_INCLUDE_DIRS})
 
 # Find libraries
@@ -41,18 +41,16 @@ find_library(TBB_DEBUG_LIBRARY tbb_debug
 #    HINTS ${PC_TBB_LIBDIR} ${PC_TBB_LIBRARY_DIRS})
 
 # Set the appropriate CMake variables and mark them as advanced
-set(TBB_INCLUDE_DIRS ${PC_TBB_INCLUDEDIR})
-set(TBB_LIBRARY_DIRS ${PC_TBB_LIBDIR})
+set(TBB_INCLUDE_DIRS ${TBB_INCLUDE_DIR})
+set(TBB_LIBRARY_DIRS ${PC_TBB_LIBRARY_DIRS})
 #set(TBB_LIBRARIES ${TBB_LIBRARY};${TBB_MALLOC_LIBRARY};${TBB_MALLOC_PROXY_LIBRARY})
 set(TBB_LIBRARIES ${TBB_LIBRARY})
 #set(TBB_DEBUG_LIBRARIES ${TBB_DEBUG_LIBRARY};${TBB_MALLOC_DEBUG_LIBRARY};${TBB_MALLOC_PROXY_DEBUG_LIBRARY})
 set(TBB_DEBUG_LIBRARIES ${TBB_DEBUG_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(tbb DEFAULT_MSG TBB_LIBRARY TBB_INCLUDE_DIR)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(tbb_debug DEFAULT_MSG TBB_DEBUG_LIBRARY TBB_INCLUDE_DIR)
+find_package_handle_standard_args(tbb DEFAULT_MSG TBB_LIBRARIES TBB_INCLUDE_DIRS)
+find_package_handle_standard_args(tbb_debug DEFAULT_MSG TBB_DEBUG_LIBRARIES TBB_INCLUDE_DIRS)
 
 mark_as_advanced(TBB_LIBRARY TBB_DEBUG_LIBRARY TBB_INCLUDE_DIR)
 #mark_as_advanced(TBB_LIBRARY TBB_DEBUG_LIBRARY TBB_MALLOC_LIBRARY TBB_DEBUG_MALLOC_LIBRARY TBB_MALLOC_PROXY_LIBRARY TBB_MALLOC_PROXY_DEBUG_LIBRARY TBB_INCLUDE_DIR)
