7ecbb09b8a4c475d1c1a08b76493a18b575aa487
SHIINA Hideaki <shiina@yndrd.com>
2012-07-07 01:21:07 +0900

    Error occurs when findNeighbors() returns no point.

--- src/cpp/flann/algorithms/nn_index.h
+++ src/cpp/flann/algorithms/nn_index.h
@@ -349,7 +349,9 @@ public:
         				count += n;
         				indices[i].resize(n);
         				dists[i].resize(n);
-        				resultSet.copy(&indices[i][0], &dists[i][0], n, params.sorted);
+						if (n > 0) {
+	        				resultSet.copy(&indices[i][0], &dists[i][0], n, params.sorted);
+						}
         			}
         		}
         		else {
@@ -363,7 +365,9 @@ public:
         				if ((int)n>params.max_neighbors) n = params.max_neighbors;
         				indices[i].resize(n);
         				dists[i].resize(n);
-        				resultSet.copy(&indices[i][0], &dists[i][0], n, params.sorted);
+						if (n > 0) {
+	        				resultSet.copy(&indices[i][0], &dists[i][0], n, params.sorted);
+						}
         			}
         		}
         	}
