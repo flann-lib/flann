3975796c9ef8fb0bf47a11266b80f60f35809a0e
Marius Muja <mariusm@cs.ubc.ca>
2012-12-14 17:36:43 -0800

    Misc changes

---
+++ bin/make_release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+VERSION=`grep "set(FLANN_VERSION" CMakeLists.txt  | sed 's/[^0-9]*\([0-9]*\.[0-9]*\.[0-9]*\)[^0-9]*/\1/'`
+
+echo "Creating flann-$VERSION-src.zip"
+
+git archive --prefix=flann-$VERSION-src/ -o flann-$VERSION-src.zip $VERSION-release
