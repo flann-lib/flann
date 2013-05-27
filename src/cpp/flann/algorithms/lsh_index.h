b72d526463e892bfc4eb30bf0c640a3cb5b9bd69
Marius Muja <mariusm@cs.ubc.ca>
2013-05-27 10:05:38 -0700

    Fixes for isses #106

--- src/cpp/flann/algorithms/lsh_index.h
+++ src/cpp/flann/algorithms/lsh_index.h
@@ -396,7 +396,7 @@ private:
         if (level == 0) return;
         for (int index = lowest_index - 1; index >= 0; --index) {
             // Create a new key
-            lsh::BucketKey new_key = key | (1 << index);
+            lsh::BucketKey new_key = key | (lsh::BucketKey(1) << index);
             fill_xor_mask(new_key, index, level - 1, xor_masks);
         }
     }
