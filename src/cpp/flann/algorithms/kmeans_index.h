1e0fcf882822068a2bd33b31a87810c0f2e2a5e9
Pierre-Emmanuel Viel <p.emmanuel.viel@gmail.com>
2013-12-20 19:11:24 +0100

    When a cluster is empty for KMeans, it's better to give it the point from another cluster j that is the furthest one from center j.

--- src/cpp/flann/algorithms/kmeans_index.h
+++ src/cpp/flann/algorithms/kmeans_index.h
@@ -658,10 +658,13 @@ private:
 
                     for (int k=0; k<indices_length; ++k) {
                         if (belongs_to[k]==j) {
-                            belongs_to[k] = i;
-                            count[j]--;
-                            count[i]++;
-                            break;
+                            // for cluster j, we move the furthest element from the center to the empty cluster i
+                            if ( distance_(points_[indices[k]], dcenters[j], veclen_) == radiuses[j] ) {
+                                belongs_to[k] = i;
+                                count[j]--;
+                                count[i]++;
+                                break;
+                            }
                         }
                     }
                     converged = false;
