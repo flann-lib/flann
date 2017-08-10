d3df5a40f9a3653aa3d9c8557ae85676912fd604
Dror Meirovich <dror@drorm.com>
2017-08-10 12:25:25 +0300

    create build_matlab_linux.sh
    In case a user wants to build/rebuild the MATLAB nearest_neighbors.mexa64 file after the FLANN library was installed.

---
+++ build_matlab_linux.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# searching for flann static library path, the search result should be successful for the mex command to work properly.
+ld -lflann_s --verbose
+mex -v -outdir src/matlab -lflann_s  -Isrc/cpp LDFLAGS='$LDFLAGS -fopenmp' CXXFLAGS='$CXXFLAGS -fopenmp -Wall -std=c++11' CXXOPTIMFLAGS='-O3 -DNDEBUG' src/matlab/nearest_neighbors.cpp
