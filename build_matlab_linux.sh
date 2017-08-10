05c345dbea58b61ce2a6cfa6ab4093d69ad752c6
Dror Meirovich <dror@drorm.com>
2017-08-10 13:05:48 +0300

    Update build_matlab_linux.sh

--- build_matlab_linux.sh
+++ build_matlab_linux.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # searching for flann static library path, the search result should be successful for the mex command to work properly.
 ld -lflann_s --verbose
+#compiling FLANN for MATLAB using the highest possible optimization level
 mex -v -outdir src/matlab -lflann_s  -Isrc/cpp LDFLAGS='$LDFLAGS -fopenmp' CXXFLAGS='$CXXFLAGS -fopenmp -Wall -std=c++11' CXXOPTIMFLAGS='-O3 -DNDEBUG' src/matlab/nearest_neighbors.cpp
