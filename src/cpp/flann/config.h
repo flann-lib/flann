a65327ab261ed289c7f795190620f77075e19984
root <tkircher@gmail.com>
2021-04-11 17:42:52 -0800

    Updated version

--- src/cpp/flann/config.h
+++ src/cpp/flann/config.h
@@ -33,7 +33,7 @@
 #ifdef FLANN_VERSION_
 #undef FLANN_VERSION_
 #endif
-#define FLANN_VERSION_ "1.9.1"
+#define FLANN_VERSION_ "1.9.2"
 
 #ifdef FLANN_VERSION_MAJOR_
 #undef FLANN_VERSION_MAJOR_
@@ -48,7 +48,7 @@
 #ifdef FLANN_VERSION_PATCH_
 #undef FLANN_VERSION_PATCH_
 #endif
-#define FLANN_VERSION_PATCH_ 1
+#define FLANN_VERSION_PATCH_ 2
 
 
 #endif /* FLANN_CONFIG_H_ */
